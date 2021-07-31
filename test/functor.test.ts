cee9abd14ebd86b0d7106ae765a1395f77d61369
Attila Domokos <adomokos@gmail.com>
2021-07-31 16:29:44 -0500

    Add functor tests for Either type

--- test/functor.test.ts
+++ test/functor.test.ts
@@ -1,4 +1,5 @@
 import { Option, isNone, none, some, map } from 'fp-ts/Option'
+import { Either, isLeft, left, right, map as eitherMap } from 'fp-ts/Either'
 
 function  lift<B, C>(g: (b: B) => C): (fb: Option<B>) => Option<C> {
     return fb => (isNone(fb) ? none : some(g(fb.value)))
@@ -17,5 +18,13 @@ describe("Functors", () => {
 
         const result2 = map<number, string>(x => x.toString() + " - oh")(none)
         expect(result2).toEqual(none)
+    })
+
+    it("can map Either values", () => {
+        const result: Either<Error, number> = eitherMap<number, number>(x => x * 2)(right(4))
+        expect(result).toEqual(right(8))
+
+        const result2: Either<Error, number> = eitherMap<number, number>(x => x * 2)(left(Error("I don't like this")))
+        expect(result2).toEqual(left(Error("I don't like this")))
     })
 })
