8ccfbaf411cef3aceabee6dd8a937b4d1475926f
Attila Domokos <adomokos@gmail.com>
2021-07-31 15:28:51 -0500

    Add a real-life monoid example

--- test/monoid.test.ts
+++ test/monoid.test.ts
@@ -1,5 +1,6 @@
 import { struct, concatAll, Monoid } from "fp-ts/Monoid"
-import { getApplyMonoid, some, none } from "fp-ts/lib/Option"
+import { last } from 'fp-ts/Semigroup';
+import { getApplyMonoid, getMonoid, Option, some, none, getLastMonoid } from "fp-ts/lib/Option"
 
 const monoidSum: Monoid<number> = {
     concat: (x, y) => x + y,
@@ -38,6 +39,31 @@ const monoidVector: Monoid<Vector> = struct({
 
 const M = getApplyMonoid(monoidSum)
 
+// Applying Monoids with real-world example
+interface Settings {
+    fontFamily: Option<string>
+    fontSize: Option<number>
+    maxColumn: Option<number>
+}
+
+const monoidSettings: Monoid<Settings> = struct({
+    fontFamily: getMonoid(last()),
+    fontSize: getMonoid(last()),
+    maxColumn: getMonoid(last())
+})
+
+const workspaceSettings: Settings = {
+    fontFamily: some('Courier'),
+    fontSize: none,
+    maxColumn: some(80)
+}
+
+const userSettings: Settings = {
+    fontFamily: some('Fira Code'),
+    fontSize: some(12),
+    maxColumn: some(80)
+}
+
 describe("Monoid", () => {
     it("can sum monoid numbers", () => {
         expect(monoidSum.concat(5, 7)).toEqual(12)
@@ -71,5 +97,16 @@ describe("Monoid", () => {
         expect(M.concat(some(1), none)).toEqual(none)
         expect(M.concat(some(1), some(2))).toEqual(some(3))
         expect(M.concat(some(1), M.empty)).toEqual(some(1))
+    })
+
+    it("can use the last Monoid", () => {
+        const result = monoidSettings.concat(workspaceSettings, userSettings)
+        const expectedSettings: Settings = {
+            fontFamily: some("Fira Code"),
+            fontSize: some(12),
+            maxColumn: some(80)
+        }
+
+        expect(result).toEqual(expectedSettings)
     })
 })
