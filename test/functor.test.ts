c7c9a1fa0e101651b9b5fa304c5c27c79b029413
Attila Domokos <adomokos@gmail.com>
2021-07-31 16:18:53 -0500

    Adding map example with Options

--- test/functor.test.ts
+++ test/functor.test.ts
@@ -1,4 +1,4 @@
-import { Option, isNone, none, some } from 'fp-ts/Option'
+import { Option, isNone, none, some, map } from 'fp-ts/Option'
 
 function  lift<B, C>(g: (b: B) => C): (fb: Option<B>) => Option<C> {
     return fb => (isNone(fb) ? none : some(g(fb.value)))
@@ -6,9 +6,16 @@ function  lift<B, C>(g: (b: B) => C): (fb: Option<B>) => Option<C> {
 
 describe("Functors", () => {
     it("can lift a function into the context", () => {
-        const o1 = some(23)
         const result = lift<number, number>(x => x * 2)(some(3))
 
         expect(result).toEqual(some(6))
+    })
+
+    it("can work with Option's map function", () => {
+        const result = map<number, string>(x => x.toString() + " - oh")(some(42))
+        expect(result).toEqual(some('42 - oh'))
+
+        const result2 = map<number, string>(x => x.toString() + " - oh")(none)
+        expect(result2).toEqual(none)
     })
 })
